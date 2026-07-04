64792dd278dff439f1658c8d40fd118c4df4e434
deeffest <117280555+deeffest@users.noreply.github.com>
2026-07-05 00:40:39 +0300

    Update config.py

--- qfluentwidgets6/common/config.py
+++ qfluentwidgets6/common/config.py
@@ -12,9 +12,6 @@ from PySide6.QtGui import QColor
 from .exception_handler import exceptionHandler
 
 
-ALERT = "\n\033[1;33m📢 Tips:\033[0m QFluentWidgets Pro is now released. Click \033[1;96mhttps://qfluentwidgets.com/pages/pro\033[0m to learn more about it.\n"
-
-
 class Theme(Enum):
     """ Theme enumeration """
 
@@ -407,10 +404,6 @@ class QConfig(QObject):
 QT_VERSION = tuple([int(v) for v in qVersion().split('.')])
 
 qconfig = QConfig()
-try:
-    print(ALERT)
-except UnicodeEncodeError:
-    print(ALERT.replace("📢", ""))
 
 
 def isDarkTheme():
