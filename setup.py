f04add0e81bb339f8702cb34e706f3ea65dbfd22
deeffest <117280555+deeffest@users.noreply.github.com>
2026-07-14 21:27:35 +0300

    Update codebase

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="PyQt5-Fluent-Widgets",
-    version="1.11.2",
+    version="1.11.2+20260714",
     packages=setuptools.find_packages(),
     install_requires=[
         "darkdetect",
